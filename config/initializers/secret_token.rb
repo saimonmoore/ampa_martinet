604152a402dba801001309904c105cd14f7885e7
Saimon Moore <saimonmoore+martinet@gmail.com>
2013-11-27 02:58:43 +0100

    Initial commit...
    Alchemy 2.7.1 Rails 3.2.15

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AmpaMartinet::Application.config.secret_token = '9db0e3e1d9618b997fdcf60a64fdcfb2f887c501a7d4162ba9692e6a69fb8f990b30759773aa928619782a4e683b8b181fd60a8a5149a5bdc2724e37183af4d0'
